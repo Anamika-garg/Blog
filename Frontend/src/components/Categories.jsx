7b9d0b677beb6ff3f2d47adae6ec481a6e333f0c
Anamika Garg <anamikagarg2906@gmail.com>
2025-01-21 22:38:15 +0530

    responsive change

--- Frontend/src/components/Categories.jsx
+++ Frontend/src/components/Categories.jsx
@@ -4,7 +4,7 @@ const Categories = () => {
   return (
     <>
     <div className="categories w-full min-h-[40px] flex items-center justify-center gap-[18px] flex-wrap">
-      <h1 className="text-2xl font-bold">Recent Blogs</h1>
+      <h1 className="text-2xl font-bold mt-[20px]">Recent Blogs</h1>
       <div className='flex items-center justify-center gap-[18px] flex-wrap'>
         <button className='text-[15px] px-4 p-2 bg-black text-white'>All</button>
         <button className='text-[15px] px-4 p-2 bg-white text-black font-semibold hover:bg-black hover:text-white transition-all'>Technology</button>
