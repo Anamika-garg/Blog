78a9470af2ab2995bdf02bc78fbdfec04c98b7da
Anamika Garg <anamikagarg2906@gmail.com>
2025-01-25 16:58:38 +0530

    vercel.json

--- Frontend/src/components/Newsletter.jsx
+++ Frontend/src/components/Newsletter.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify';
 
 const Newsletter = () => {
+    const [email , setEmail] = useState('');
+    const inputHandler = (e) =>{
+        setEmail(e.target.value);
+    }
+    const submitHandler = (e) =>{
+        e.preventDefault();
+        setEmail('');
+        toast.success('Success');
+    }
     return (
         <>
             <div className="container m-auto w-[80vw] min-h-[50vh] flex items-center justify-center flex-col gap-[26px]">
+                <ToastContainer/>
                 <h1 className="text-4xl font-bold md:mt-16 mt-[88px] ">
                     Latest Blogs
                 </h1>
                 <p className='text-center'>Stay updated with the latest news, tips, and exclusive content delivered straight to your inbox.<br></br> Don't miss out!</p>
-                <div className="take-input w-[50%] relative flex items-center justify-center min-h-[40px] flex-wrap">
-                    <input type="text" placeholder='Enter Your Email Address' className='p-3 w-[400px] relative border-2 border-black'/>
-                    <button className='py-3 px-4 w-[100px] bg-black text-white border-2 border-black mt-[9px] md:mt-[0] rounded-md'>Subscribe</button>
+                <div className="take-input w-[50%] relative flex items-center justify-center min-h-[40px] flex-wrap gap-[1px]">
+                    <input type="text" placeholder='Enter Your Email Address' className='p-3 w-[400px] relative border-2 border-black' name='email' value={email} onChange={inputHandler}/>
+                    <button className='py-3 px-4 w-[100px] bg-black text-white border-2 border-black mt-[9px] md:mt-[0] rounded-md' onClick={submitHandler}>Subscribe</button>
                 </div>
             </div>
         </>
